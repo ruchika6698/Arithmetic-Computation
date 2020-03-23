fed8a8781f0e9241b7abab233a67f3b074024eec
ruchika <ruchika6698@gmail.com>
2020-03-23 18:54:33 +0530

    Added:Three User Inputs

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -x
 
 printf "----------------Arithmetic Computation and Sorting---------------\n"
+
+read -p "Enter number a:" a
+read -p "Enter number b:" b
+read -p "Enter number c:" c
