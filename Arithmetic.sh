080b684fbd2ec6c7bfa66730025583d6af70930c
ruchika <ruchika6698@gmail.com>
2020-03-23 19:18:29 +0530

    Added:Store the all operation result in Dictionary

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -2,18 +2,28 @@
 
 printf "----------------Arithmetic Computation and Sorting---------------\n"
 
+#Initialise Dictionary
+declare -A dictionary
+dictionary=( ["Firstoperation"]=0 ["Secondoperation"]=0 ["Thirdoperation"]=0 ["Forthoperation"]=0 )
+
 read -p "Enter number a:" a
 read -p "Enter number b:" b
 read -p "Enter number c:" c
 
 x=$(( $a + $b * $c ))
 printf "First Operation: $x \n"
+dictionary[Firstoperation]=$x
 
 u=$((	$a * $b + $c ))
 printf "Second Operation: $u \n"
+dictionary[Secondoperation]=$u
 
 z=$(( $c + $a / $b ))
 printf "Third Operation: $z \n"
+dictionary[Thirdoperation]=$z
 
 y=$(( $a % $b + $c ))
 printf "Fourth Operation: $y \n"
+dictionary[Forthoperation]=$y
+
+echo ${!dictionary[@]} ' : ' ${dictionary[@]}
