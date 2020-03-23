6774a9297343ea4e7623b777a74eb2812fdac0f9
ruchika <ruchika6698@gmail.com>
2020-03-23 19:00:03 +0530

    Added:First Operation a+b*c

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -5,3 +5,7 @@ printf "----------------Arithmetic Computation and Sorting---------------\n"
 read -p "Enter number a:" a
 read -p "Enter number b:" b
 read -p "Enter number c:" c
+
+x=$(( $a + $b * $c ))
+printf "First Operation: $x \n"
+
